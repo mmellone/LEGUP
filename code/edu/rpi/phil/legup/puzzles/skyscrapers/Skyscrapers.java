0a9d0fff049edf3f45c23fdd0871467983d1f637
Anthony-Handwerker <antrog.werker@yahoo.com>
2014-10-12 22:22:23 -0400

    Skyscrapers.java created

--- code/edu/rpi/phil/legup/puzzles/skyscrapers/Skyscrapers.java
+++ code/edu/rpi/phil/legup/puzzles/skyscrapers/Skyscrapers.java
@@ -1,1 +1,31 @@
 package edu.rpi.phil.legup.puzzles.skyscrapers;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import javax.swing.JFrame;
+import javax.swing.JDialog;
+
+import edu.rpi.phil.legup.AI;
+import edu.rpi.phil.legup.BoardImage;
+import edu.rpi.phil.legup.BoardState;
+import edu.rpi.phil.legup.CaseRule;
+import edu.rpi.phil.legup.Contradiction;
+import edu.rpi.phil.legup.PuzzleGeneration;
+import edu.rpi.phil.legup.PuzzleModule;
+import edu.rpi.phil.legup.PuzzleRule;
+
+public class Skyscrapers extends PuzzleModule
+{
+  
+}
