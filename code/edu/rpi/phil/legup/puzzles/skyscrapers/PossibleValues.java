072f88b508710e8921a36a7b8472596e9efc7b91
Anthony-Handwerker <antrog.werker@yahoo.com>
2014-10-21 14:31:50 -0400

    Added PossibleValues case rule.

---
+++ code/edu/rpi/phil/legup/puzzles/skyscrapers/PossibleValues.java
@@ -0,0 +1,14 @@
+
+
+package edu.rpi.phil.legup.puzzles.skyscrapers;
+import edu.rpi.phil.legup.BoardState;
+import edu.rpi.phil.legup.CaseRule;
+
+public class PossibleValues extends CaseRule
+{
+  public PossibleValues()
+  {
+    setName("Possible Values");
+    description = "Each cell has a value between 1 and N.";
+  }
+}
