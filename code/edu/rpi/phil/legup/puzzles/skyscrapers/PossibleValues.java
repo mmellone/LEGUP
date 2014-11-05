731fab241fc853895e5e3764336cb4e7e29270bc
Anthony-Handwerker <antrog.werker@yahoo.com>
2014-11-04 20:46:37 -0500

    Adjusted indentation for consistency

--- code/edu/rpi/phil/legup/puzzles/skyscrapers/PossibleValues.java
+++ code/edu/rpi/phil/legup/puzzles/skyscrapers/PossibleValues.java
@@ -1,14 +1,12 @@
-
-
 package edu.rpi.phil.legup.puzzles.skyscrapers;
 import edu.rpi.phil.legup.BoardState;
 import edu.rpi.phil.legup.CaseRule;
 
 public class PossibleValues extends CaseRule
 {
-  public PossibleValues()
-  {
-    setName("Possible Values");
-    description = "Each cell has a value between 1 and N.";
-  }
+	public PossibleValues()
+	{
+		setName("Possible Values");
+		description = "Each cell has a value between 1 and N.";
+	}
 }
--- code/edu/rpi/phil/legup/puzzles/skyscrapers/Skyscrapers.java
+++ code/edu/rpi/phil/legup/puzzles/skyscrapers/Skyscrapers.java
@@ -27,125 +27,125 @@ import edu.rpi.phil.legup.PuzzleRule;
 
 public class Skyscrapers extends PuzzleModule
 {
-  public static int CELL_UNKNOWN = 0; //rewritten for clarity
-  private int boardSize = 0;
-  
-  private Vector<PuzzleRule> puzzleRules = new Vector<PuzzleRule>();
-  private Vector<Contradiction> contraRules = new Vector<Contradiction>();
-  private Vector<CaseRule> caseRules = new Vector<CaseRule>();
-  
-  public int numAcceptableStates() { return boardSize + 1; }
-  
-  public Skyscrapers()
-  {
-    name = "Skyscrapers";
-  }
-  
-  /**
-   * Creates a BoardState from an input file.
-   *
-   * @param filename the location of the file containing the boardstate
-   * @return the BoardState described by the file.
-   **/
-  public BoardState importPuzzle(String filename)
-  {
-    BoardState ret = super.importPuzzle(filename);
-    if(ret != null)
-    {
-      boardSize = ret.getWidth();
-    }
-    else
-    {
-      boardSize = 0;
-    }
-    return ret;
-  }
-  
-  /**
-   * Get the next call value of all of them (so if we're editing tree tent, for example, we can
-   * change to and from trees)
-   *
-   * @param x Column coordinate of the cell
-   * @param y Row coordinate of the cell
-   * @param boardState BoardState that the cell should be looked up in
-   * @return The next cell value
-   */
-  public int getAbsoluteNextCellValue(int x, int y, BoardState boardState)
-  {
-    int ret = boardState.getCellContents(x,y);
-    ret = (ret + 1) % numAcceptableStates();
-    return ret;
-  }
-  
-  
-  /**
-   * Gets the next cell value for a specified cell in a boardstate
-   *
-   * @param x Column coordinate of the cell
-   * @param y Row coordinate of the cell
-   * @param boardState BoardState that the cell should be looked up in
-   * @return The next cell value
-   */
-  public int getNextCellValue(int x, int y, BoardState boardState)
-  {
-    int ret = boardState.getCellContents(x,y);
-    ret = (ret + 1) % numAcceptableStates();
-    return ret;
-  }
-  
-  /**
-   * Gets the name of a state from its number.
-   *
-   * @param state the state number
-   * @return the name of the state
-   **/
-  public String getStateName(int state)
-  {
-    if(state != 0)
-    {
-      return state + "";
-    }
-    else
-    {
-      return "";
-    }
-  }
-  
-  /**
-   * Gets the number of a state from its name.
-   *
-   * @param state the state name.
-   * @return the state number.
-   **/
-  public int getStateNumber(String state)
-  {
-    if(state.equals(""))
-    {
-      return 0;
-    }
-    else
-    {
-      return Integer.parseInt(state);
-    }
-  }
-  
-  /**
-   * Returns a list of the Basic Rules.
-   *
-   * @return a list of the basic rules.
-   **/
-  public Vector<PuzzleRule> getRules()
-  {
-    return puzzleRules;
-  }
-  
-  public Vector <Contradiction> getContradictions()
-  {
-    return contraRules;
-  }
-  
-  public Vector<CaseRule> getCaseRules()
-  {
-    return caseRules;
-  }
+	public static int CELL_UNKNOWN = 0; //rewritten for clarity
+	private int boardSize = 0;
+	
+	private Vector<PuzzleRule> puzzleRules = new Vector<PuzzleRule>();
+	private Vector<Contradiction> contraRules = new Vector<Contradiction>();
+	private Vector<CaseRule> caseRules = new Vector<CaseRule>();
+	
+	public int numAcceptableStates() { return boardSize + 1; }
+	
+	public Skyscrapers()
+	{
+		name = "Skyscrapers";
+	}
+	
+	/**
+	 * Creates a BoardState from an input file.
+	 *
+	 * @param filename the location of the file containing the boardstate
+	 * @return the BoardState described by the file.
+	 **/
+	public BoardState importPuzzle(String filename)
+	{
+		BoardState ret = super.importPuzzle(filename);
+		if(ret != null)
+		{
+			boardSize = ret.getWidth();
+		}
+		else
+		{
+			boardSize = 0;
+		}
+		return ret;
+	}
+	
+	/**
+	 * Get the next call value of all of them (so if we're editing tree tent, for example, we can
+	 * change to and from trees)
+	 *
+	 * @param x Column coordinate of the cell
+	 * @param y Row coordinate of the cell
+	 * @param boardState BoardState that the cell should be looked up in
+	 * @return The next cell value
+	 */
+	public int getAbsoluteNextCellValue(int x, int y, BoardState boardState)
+	{
+		int ret = boardState.getCellContents(x,y);
+		ret = (ret + 1) % numAcceptableStates();
+		return ret;
+	}
+	
+	
+	/**
+	 * Gets the next cell value for a specified cell in a boardstate
+	 *
+	 * @param x Column coordinate of the cell
+	 * @param y Row coordinate of the cell
+	 * @param boardState BoardState that the cell should be looked up in
+	 * @return The next cell value
+	 */
+	public int getNextCellValue(int x, int y, BoardState boardState)
+	{
+		int ret = boardState.getCellContents(x,y);
+		ret = (ret + 1) % numAcceptableStates();
+		return ret;
+	}
+	
+	/**
+	 * Gets the name of a state from its number.
+	 *
+	 * @param state the state number
+	 * @return the name of the state
+	 **/
+	public String getStateName(int state)
+	{
+		if(state != 0)
+		{
+			return state + "";
+		}
+		else
+		{
+			return "";
+		}
+	}
+	
+	/**
+	 * Gets the number of a state from its name.
+	 *
+	 * @param state the state name.
+	 * @return the state number.
+	 **/
+	public int getStateNumber(String state)
+	{
+		if(state.equals(""))
+		{
+			return 0;
+		}
+		else
+		{
+			return Integer.parseInt(state);
+		}
+	}
+	
+	/**
+	 * Returns a list of the Basic Rules.
+	 *
+	 * @return a list of the basic rules.
+	 **/
+	public Vector<PuzzleRule> getRules()
+	{
+		return puzzleRules;
+	}
+	
+	public Vector <Contradiction> getContradictions()
+	{
+		return contraRules;
+	}
+	
+	public Vector<CaseRule> getCaseRules()
+	{
+		return caseRules;
+	}
 }
