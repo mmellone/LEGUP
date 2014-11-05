6053c05ad22be758d05a5044ad8fd9f8a38c24cd
Anthony-Handwerker <antrog.werker@yahoo.com>
2014-11-04 21:57:18 -0500

    Added serial version uid

--- code/edu/rpi/phil/legup/puzzles/skyscrapers/PossibleValues.java
+++ code/edu/rpi/phil/legup/puzzles/skyscrapers/PossibleValues.java
@@ -4,6 +4,7 @@ import edu.rpi.phil.legup.CaseRule;
 
 public class PossibleValues extends CaseRule
 {
+	static final long serialVersionUID = 701735L
 	public PossibleValues()
 	{
 		setName("Possible Values");
