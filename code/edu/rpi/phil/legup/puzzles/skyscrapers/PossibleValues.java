a56f88c87f2968894a293096d735adc6d49f8582
Anthony-Handwerker <antrog.werker@yahoo.com>
2014-11-04 21:58:26 -0500

    Semicolon.

--- code/edu/rpi/phil/legup/puzzles/skyscrapers/PossibleValues.java
+++ code/edu/rpi/phil/legup/puzzles/skyscrapers/PossibleValues.java
@@ -4,7 +4,7 @@ import edu.rpi.phil.legup.CaseRule;
 
 public class PossibleValues extends CaseRule
 {
-	static final long serialVersionUID = 701735L
+	static final long serialVersionUID = 701735L;
 	public PossibleValues()
 	{
 		setName("Possible Values");
